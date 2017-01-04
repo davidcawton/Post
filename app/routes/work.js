97cd6b8d6840ec96a0357a078292dcc0607db56a
David Cawton <dcawton@gmail.com>
2017-01-04 16:15:12 -0500

    Added Photo Album

--- app/routes/work.js
+++ app/routes/work.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return [
+      { thumbUrl: '../one.jpg' },
+      { thumbUrl: '../two.jpg' },
+      { thumbUrl: '../three.jpg' },
+      { thumbUrl: '../four.jpg' },
+      { thumbUrl: '../five.jpg' },
+      { thumbUrl: '../six.jpg' },
+    ];
+  },
 });
